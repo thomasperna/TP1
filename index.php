e9d3fe4ea14f82e0a0f476373f41093da8063610
thoma <thoma@172.16.0.89>
2017-09-12 13:44:39 +0200

    test

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/csstp1.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Ceci est le tp1</h1>
+    </body>
+</html>
